4098772ab6ed3427cce6b8a5a1c2abc713d59bb8
Hysao <rickyjoe.enrile29@gmail.com>
2024-09-21 16:40:07 +0800

    First commit initial work

---
+++ admin/src/main/java/com/myprograms/admin/AdminActivity.java
@@ -0,0 +1,32 @@
+package com.myprograms.admin;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class AdminActivity extends AppCompatActivity {
+
+    private EditText userName, password;
+    private Button login;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_admin);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+
+
+    }
+}

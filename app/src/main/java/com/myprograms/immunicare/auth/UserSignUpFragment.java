abfc01f0c35504cb29251c65b65b86ef8408a258
Hysao <rickyjoe.enrile29@gmail.com>
2024-10-13 21:44:09 +0800

    added just the calendar without anything yet

--- app/src/main/java/com/myprograms/immunicare/auth/UserSignUpFragment.java
+++ app/src/main/java/com/myprograms/immunicare/auth/UserSignUpFragment.java
@@ -1,5 +1,6 @@
 package com.myprograms.immunicare.auth;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,16 +8,35 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.myprograms.immunicare.R;
+import com.myprograms.immunicare.calendar.CalendarActivity;
 
 
 public class UserSignUpFragment extends Fragment {
 
+    private Button validateButton;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_user_sign_up, container, false);
+        View view = inflater.inflate(R.layout.fragment_user_sign_up, container, false);
+
+        validateButton = view.findViewById(R.id.userValidateButton);
+
+        // Now you can work with your views, like setting click listeners
+        validateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Handle button click here
+                Intent intent = new Intent(getActivity(), CalendarActivity.class);
+
+                startActivity(intent);
+            }
+        });
+
+        return view; // Return the inflated view
+
     }
 }
